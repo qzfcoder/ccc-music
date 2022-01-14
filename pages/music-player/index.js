6daa0d1c1a9872164dd9173ab228ae1b9ddee8c0
qzfcoder <867303253@qq.com>
2022-01-14 17:31:15 +0800

    x

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -5,6 +5,9 @@ import {
 import {
   NavBarHeight
 } from '../../constants/device-const'
+import {
+  audioContext
+} from '../../store/index'
 Page({
 
   /**
@@ -13,7 +16,11 @@ Page({
   data: {
     currentSong: {},
     currentPage: 0,
-    contentHeight: 0
+    contentHeight: 0,
+    isMusicLyric: true,
+    duration: 0,
+    currentTime: 0,
+    sliderValue: 0
   },
 
   /**
@@ -28,25 +35,44 @@ Page({
     // 动态计算高度
     const screenHeight = getApp().globalData.screenHeight
     const statusBarHeight = getApp().globalData.statusBarHeight
+    const screenWidth = getApp().globalData.screenWidth
     const contentHeight = screenHeight - statusBarHeight - NavBarHeight
+    if (screenHeight / screenWidth < 1.8) {
+      this.setData({
+        isMusicLyric: false
+      })
+    }
     this.setData({
       contentHeight: contentHeight
     })
 
-    // 4.创建播放器
-    const audioContext = wx.createInnerAudioContext()
+    // 4.使用audioContext播放歌曲
+    audioContext.stop()
     audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
-    audioContext.play()
-    setTimeout(() => {
-      audioContext.pause()
-    }, 5000);
+    audioContext.autoplay = true
+    audioContext.stop()
+    audioContext.onCanplay(() => {
+      // audioContext.play()
+      // audioContext.stop()
+    })
+
+    audioContext.onTimeUpdate(() => {
+      const currentTime = audioContext.currentTime * 1000
+      this.setData({
+        currentTime
+      })
+    })
+
+    // 5.audioContext的事件监听
+    // this.setupAudioContextListener()
   },
   // 网络请求
   getPageDate: function (id) {
     getSongDetail(id).then(res => {
       console.log(res)
       this.setData({
-        currentSong: res.songs[0]
+        currentSong: res.songs[0],
+        duration: res.songs[0].dt
       })
     })
   },
@@ -54,5 +80,15 @@ Page({
     this.setData({
       currentPage: event.detail.current
     })
+  },
+  handleSliderChange: function (event) {
+    const value = event.detail.value
+    const rate = value / 100
+    this.setData({
+      currentTime: this.data.duration * rate,
+      // sliderValue: value
+    })
+    audioContext.pause()
+    audioContext.seek(this.data.currentTime / 1000)
   }
 })
--- store/index.js
+++ store/index.js
@@ -1,5 +1,10 @@
-import { rankingStore } from './ranking-store'
-
-export {
+import {
   rankingStore
+} from './ranking-store'
+import {
+  audioContext
+} from './player-store'
+export {
+  rankingStore,
+  audioContext
 }
